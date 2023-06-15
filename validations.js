6c76e30856646b21bf144d04bccbffcc9fcc3f7e
BroDi4 <xrip73@mail.ru>
2023-06-15 17:42:09 +0400

    fix

--- validations.js
+++ validations.js
@@ -2,8 +2,8 @@ import { body } from 'express-validator';
 
 export const registerValidation = [
   body('email', 'Неправильный формат почты').isEmail(),
-  body('name', 'Имя должно состоять из букв, минимальная длина имени 2 символа')
-    .isAlpha()
-    .isLength({ min: 2 }),
+  body('name', 'Имя должно состоять из букв, минимальная длина имени 2 символа').isLength({
+    min: 2,
+  }),
   body('password', 'Минимальная длина пароля 5 символов').isLength({ min: 5 }),
 ];
